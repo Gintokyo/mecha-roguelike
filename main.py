595cc88a3e3871dfe9aaca31786c558e64be2fe6
Gintokyo <153867001+Gintokyo@users.noreply.github.com>
2026-09-14 16:15:33 +0100

    Started creating the Mech class

---
+++ main.py
@@ -0,0 +1,6 @@
+from mech import Mech
+
+my_mech = Mech("Atlas")
+
+print(my_mech.name)
+print(my_mech.stats)
---
+++ mech.py
@@ -0,0 +1,8 @@
+class Mech:
+    def __init__(self, name):
+        self.name = name
+        self.stats = {
+            "HP": 100,
+            "ATK": 10,
+            "DEF": 5
+        }
